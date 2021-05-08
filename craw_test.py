cf20b5d8e056a5fbc8708ab72432e0dffa01aed0
Boyzmsc <boyzmsc@kookmin.ac.kr>
2021-05-08 18:56:51 +0900

    Apply selenium

--- craw_test.py
+++ craw_test.py
@@ -1,26 +1,57 @@
+from selenium import webdriver
 import requests
 from bs4 import BeautifulSoup
 
-login_url = 'https://ecampus.kookmin.ac.kr/login/index.php'
-craw_url = 'https://ecampus.kookmin.ac.kr/'
+browser = webdriver.Chrome("chromedriver")
 
-session = requests.session()
+# 1. 로그인 페이지 이동
+browser.get("https://ecampus.kookmin.ac.kr/login/index.php")
 
-params = dict()
-params['m_id'] = 'boyzmsc'
-params['m_password'] = 'qwerasdf0101^^'
+# 2. 로그인
+browser.find_element_by_id("input-username").send_keys("boyzmsc")
+browser.find_element_by_id("input-password").send_keys("qwerasdf0101^^")
 
-login = session_data.post(login_url, data = params)
-login.raise_for_status()
- 
-#print(login.headers)
-#print(session_data.cookies.get_dict())
- 
-login = session_data.get(craw_url)
- 
-soup = BeautifulSoup(login.content, 'html.parser')
- 
-data = soup.select('dl.mileage_section1 > dd > span')
+browser.find_element_by_name("loginbutton").click()
 
-for item in data:
-    print (item.get_text())
+# 3. 강의-과제 페이지
+course_ids = browser.find_element_by_css_selector(
+    "#page-content > div > div > section.col-lg-8.col-xl-9.col-dash > div > div > div > div.my-course-lists > div > div"
+)
+
+# browser.find_element_by_css_selector(
+#     "#page-content > div > div > section.col-lg-8.col-xl-9.col-dash > div > div > div > div.my-course-lists > div > div > div:nth-child(7) > a"
+# ).click()
+
+# # 4. 과제 페이지
+# browser.find_element_by_css_selector(
+#     "#module-activities > ul > li:nth-child(2) > a"
+# ).click()
+
+
+# login_url = "https://ecampus.kookmin.ac.kr/login/index.php"
+# craw_url = "https://ecampus.kookmin.ac.kr/calendar/view.php?view=month&cal_m=05"
+
+# session = requests.session()
+
+# params = dict()
+# params["loginId"] = "boyzmsc"
+# params["loginPwd"] = "qwerasdf0101^^"
+
+# login = session.post(login_url, data=params)
+# login.raise_for_status()
+
+# # print(login.headers)
+# # print(session.cookies.get_dict())
+
+# login = session.get(craw_url)
+
+# soup = BeautifulSoup(login.text, "html.parser")
+
+# data = soup.select_one(
+#     "#month-detailed-6096262cb40ed6096262c1b5ad2 > tbody > tr:nth-child(2) > td:nth-child(3) > div.d-none.d-md-block.hidden-phone.text-xs-center > div > ul > li:nth-child(1) > a > span.eventname"
+# )
+
+# print(data.get_text())
+
+# for item in data:
+#     print(item.get_text())
