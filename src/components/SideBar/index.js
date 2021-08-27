01086f66325dc3cca6c678d2c502472d6851e1ec
MSC <boyzmsc@kookmin.ac.kr>
2021-08-28 04:29:41 +0900

    feat: Add Firebase & Firestore

--- src/components/SideBar/index.js
+++ src/components/SideBar/index.js
@@ -1,12 +1,13 @@
 /* eslint-disable react/style-prop-object */
-import React, { Component } from "react";
-import { Link, withRouter } from 'react-router-dom';
+import React, {Component} from "react";
+import {Link, withRouter} from "react-router-dom";
 
 import {
   FaTachometerAlt,
   FaRegCalendarAlt,
   FaRegListAlt,
-  FaGraduationCap
+  FaGraduationCap,
+  FaInfoCircle,
 } from "react-icons/fa";
 
 import {
@@ -19,37 +20,49 @@ import {
 } from "react-pro-sidebar";
 
 import "react-pro-sidebar/dist/css/styles.css";
-import "./sidebar.scss"
+import "./SideBar.scss";
 
 class SideBar extends Component {
   render() {
     return (
       <>
-        <ProSidebar className = "sidebar-main">
-          <SidebarHeader className = "sidebar-header">
-            <a href = "/dashboard" className = "home-text">Check My Ass</a>
+        <ProSidebar className="sidebar-main">
+          <SidebarHeader className="sidebar-header">
+            <a href="/dashboard" className="home-text">
+              Check My Ass
+            </a>
           </SidebarHeader>
-          <SidebarContent>
+          <SidebarContent className="sidebar-content">
             <Menu iconShape="circle">
-              <MenuItem className = "menuitem" icon={<FaTachometerAlt />}>
+              <MenuItem className="menuitem" icon={<FaTachometerAlt />}>
                 <Link to="/dashboard">Dashboard</Link>
               </MenuItem>
 
-              <MenuItem className = "menuitem" icon={<FaGraduationCap />}>
+              <MenuItem className="menuitem" icon={<FaGraduationCap />}>
                 <Link to="/assignment">Assignment</Link>
               </MenuItem>
 
-              <MenuItem className = "menuitem" icon={<FaRegListAlt />}>
+              <MenuItem className="menuitem" icon={<FaRegListAlt />}>
                 <Link to="/task">Task</Link>
               </MenuItem>
 
-              <MenuItem className = "menuitem" icon={<FaRegCalendarAlt />}>
+              <MenuItem className="menuitem" icon={<FaRegCalendarAlt />}>
                 <Link to="/calendar">Calendar</Link>
+              </MenuItem>
+
+              <MenuItem className="menuitem" icon={<FaInfoCircle />}>
+                <Link to="/info">Info</Link>
               </MenuItem>
             </Menu>
           </SidebarContent>
-          <SidebarFooter className = "sidebar-footer">
-          <a href = "https://ecampus.kookmin.ac.kr/" target = "_blank" className = "univ-text">Kookmin University</a>
+          <SidebarFooter className="sidebar-footer">
+            <a
+              href="https://ecampus.kookmin.ac.kr/"
+              target="_blank"
+              className="univ-text"
+            >
+              Kookmin University
+            </a>
           </SidebarFooter>
         </ProSidebar>
       </>
@@ -57,4 +70,4 @@ class SideBar extends Component {
   }
 }
 
-export default withRouter(SideBar);
+export default SideBar;
